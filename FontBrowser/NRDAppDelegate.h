8baf9bbee93f3716f352b7397623e0307112cb8a
Joshua Sullivan <jsulliva@nerdery.com>
2013-11-04 14:30:34 -0600

    Initial Commit

---
+++ FontBrowser/NRDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  NRDAppDelegate.h
+//  FontBrowser
+//
+//  Created by Joshua Sullivan on 11/4/13.
+//  Copyright (c) 2013 The Nerdery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FontBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FontBrowser
+//
+//  Created by Joshua Sullivan on 11/4/13.
+//  Copyright (c) 2013 The Nerdery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NRDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NRDAppDelegate class]));
+    }
+}
